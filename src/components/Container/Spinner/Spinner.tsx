bf00908412f7d094a0886c760989c641994293fb
AbrahamGalvezV <2abrahamtattoosk8@gmail.com>
2026-07-07 17:32:06 +0200

    change img

--- src/components/Container/Spinner/Spinner.tsx
+++ src/components/Container/Spinner/Spinner.tsx
@@ -1,11 +1,13 @@
 import styles from "./Spinner.module.css"
+import pokemon from "../../../assets/img/pokemon.png"
+import pokeball from "../../../assets/img/pokeball.png"
 
 export const Spinner = () => {
 
   return (
     <div className="text-center">
-      <img src="../img/pokemon.png" className={styles.spinner}/>
-      <img src="../img/pokeball.png" className={styles.spinner_pokeball}/>
+      <img src={pokemon} className={styles.spinner}/>
+      <img src={pokeball} className={styles.spinner_pokeball}/>
     </div>
   );
 };
